068fd71f23ec6308fab15a52226ab33f3a286d66
Vanessal87l <lissss667@gmail.com>
2026-01-09 01:17:11 +0500

    navbar2 bn sotuvlarni qowiw bitgani

---
+++ src/pages/components/ReactSelect.tsx
@@ -0,0 +1,35 @@
+import Select from "react-select";
+
+const options = [
+  { label: "2000", value: "2000" },
+  { label: "1500", value: "1500" },
+  { label: "5500", value: "5500" },
+];
+
+export function ReactSelectBox() {
+  return (
+    <div className="w-full">
+      <Select
+        options={options}
+        placeholder="Tanlang..."
+        classNamePrefix="rs"
+        styles={{
+          control: (base, state) => ({
+            ...base,
+            maxWidth: "494px",
+            minHeight: "51px",
+            borderRadius: "12px",
+            border: state.isFocused ? "1px solid #6049E3" : "1px solid #D0D0D0",
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: "6px",
+            paddingRight: "16px",
+            boxShadow: "none",
+            outline: "none",
+            background: "#EBF0FA",
+          }),
+        }}
+      />
+    </div>
+  );
+}
--- src/widgets/topbar_2/Topbar2.tsx
+++ src/widgets/topbar_2/Topbar2.tsx
@@ -1,3 +1,31 @@
-export default function Topbar2() {
-  return <div className="text-white">Topbar2</div>;
+import { cn } from "@/lib/utils";
+import { NavLink } from "react-router-dom";
+
+const tabs = [
+  { label: "Sotuv qo'shish", to: "/SotuvQoshish" },
+  { label: "Sotuvlar ro'yhati", to: "/SotuvlarRoyhati" },
+  { label: "Tolov oynasi", to: "/TolovOynasi" },
+  { label: "Qaytarilgan tovarlar", to: "/QaytarilganTovarlar" },
+] as const;
+
+export default function Navbar2() {
+  return (
+    <nav className=" w-full max-w-[1402px] h-auto flex gap-2 border border-[#6049E3] rounded-3xl px-3 py-2 bg-muted mt-4">
+      {tabs.map((t) => (
+        <NavLink
+          to={t.to}
+          key={t.to}
+          className={({ isActive }) =>
+            cn(
+              "px-3 rounded-2xl text-sm font-medium  transition-all   flex items-center duration-200 h-7 ",
+              !isActive && "text-black",
+              isActive && "navbar2-button-color"
+            )
+          }
+        >
+          {t.label}
+        </NavLink>
+      ))}
+    </nav>
+  );
 }
